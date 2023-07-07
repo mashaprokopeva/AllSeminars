f9d7e09c6dc9d4c888cd9f8fe99bc53cc6bf05ee
Evgeny Norkin <norkinzheny@gmail.com>
2023-07-07 17:23:37 +0300

    hw complete

---
+++ task36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int a = 5;
+int[] tempArray = new int[a];
+void mas(int a)
+{
+    
+    for (int i = 0; i < a; i++)
+    {
+        tempArray[i] = new Random().Next(1,9);
+        Console.Write(tempArray[i] + ",");
+    }
+}
+
+int Kol(int[] tempArray)
+{
+int sum = 0;
+int i = 0;
+while (i < tempArray.Length)
+{
+    sum = sum + tempArray[i];
+    i = i + 2;
+}
+return sum;
+}
+
+mas(a);
+Console.Write($"\nСумма элементов, стоящих на нечётных позициях: {Kol(tempArray)}");
+
+
---
+++ task38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+//Console.Write("Введите количество элементов массива: ");
+int a = 5;
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+Random rand = new Random();
+
+for (int i = 0; i < a; i++)
+{
+
+//randomArray[i] = rand.NextDouble();
+randomArray[0] = 3.22;
+randomArray[1] = 5;
+randomArray[2] = 3.22;
+randomArray[3] = 1.2;
+randomArray[4] = 3.23;
+Console.Write($"{randomArray[i]:F2}" + ".");
+}
+
+}
+
+double raz(double[] randomArray)
+{
+double min = randomArray[0];
+double max = randomArray[0];
+int i = 1;
+while (i < randomArray.Length)
+{
+if (max<randomArray[i])
+max = randomArray[i];
+if (min>randomArray[i])
+min = randomArray[i];
+i = i + 1;
+}
+return max-min;
+}
+
+mas(a);
+Console.Write($"\nРазница между максимальным и минимальным элементов массива: {raz(randomArray):F2}");
